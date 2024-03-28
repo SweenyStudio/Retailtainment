0f155ea049d3804e233abc8af54e58b019851f62
Mac Sweeny <macsweenydev@gmail.com>
2024-03-28 15:20:38 +1000

    Refactor code for improved performance and readability

---
+++ src/components/sections/flywheel.tsx
@@ -0,0 +1,15 @@
+import Container from "@/components/ui/container";
+import { SubHeader } from "../ui/text";
+
+const FlyWheelObjects = []
+
+export default function Flywheel() {
+    return (
+        <section>
+            <Container>
+                Introducing the
+                <SubHeader className="text-center">Profitable Scaling Flywheel</SubHeader>
+            </Container>
+        </section>
+    );
+}
