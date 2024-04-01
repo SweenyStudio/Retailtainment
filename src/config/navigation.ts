df4bf7e835c1acfadd4911cb83c06f28c39ae4ba
Mac Sweeny <macsweenydev@gmail.com>
2024-04-01 10:56:18 +1000

    Refactor navigation and useDimensions code

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -1,16 +1,14 @@
-import { PersonIcon } from "@radix-ui/react-icons";
-
 export const mainMenu = [
   {
-    title: "Home",
-    path: "/",
+    title: 'Home',
+    path: '/',
   },
   {
-    title: "About Us",
-    path: "/about",
+    title: 'About Us',
+    path: '/about',
   },
   {
-    title: "Login",
-    path: "https://www.skool.com/retailtainment/about",
+    title: 'Login',
+    path: 'https://www.skool.com/retailtainment/about',
   },
 ];
