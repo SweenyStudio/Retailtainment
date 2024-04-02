9c40820643c80b4519e852f7a087a701d8fdb61e
Mac Sweeny <macsweenydev@gmail.com>
2024-04-02 15:22:03 +1000

    typeform issues lol

--- src/app/apply/page.tsx
+++ src/app/apply/page.tsx
@@ -2,8 +2,8 @@
 
 import Navbar from '@/components/sections/navbar';
 import { sendGTMEvent } from '@next/third-parties/google';
+import { Widget } from '@typeform/embed-react';
 import { usePathname } from 'next/navigation';
-import Script from 'next/script';
 import { useEffect } from 'react';
 
 export default function Apply() {
@@ -19,12 +19,7 @@ export default function Apply() {
   return (
     <div className="bg-black">
       <Navbar />
-
-      <div
-        className="min-h-[650px] bg-white"
-        data-tf-live="01HTEJR1GW5JCWPEY2P3J5KS7B"
-      ></div>
-      <Script src="//embed.typeform.com/next/embed.js"></Script>
+      <Widget id="CpKOxivA" className="min-h-[650px] bg-white" />
     </div>
   );
 }
